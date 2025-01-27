db17de456e900f6f4ecb686eec0e9ade68032ff7
mefazdev <fazdev.com@gmail.com>
2025-01-27 22:22:10 +0530

    fixapi-4

--- app/api/news/[id]/view/route.js
+++ app/api/news/[id]/view/route.js
@@ -1,31 +1,28 @@
  
 import { ObjectId } from 'mongodb';
 import clientPromise from '../../../../lib/mongodb';
-// import clientPromise from '@/lib/mongodb';  
 
-// The API route handler
 export async function GET(req,{ params }) {
   try {
  
-    // Extract the ID from the request parameters
+  
     const { id } = params;
   
     const objectId = ObjectId.createFromHexString(id)
-    // Connect to MongoDB
+    
     const client = await clientPromise;
-    const db = client.db('wespark'); // Replace with your database name
-
-    // Fetch the document by _id
+    const db = client.db('wespark');  
+ 
     const pkg = await db.collection('news').findOne({ _id: objectId });
 
-    // If the document is not found, return a 404
+     
     if (!pkg) {
       return new Response(JSON.stringify({ error: 'Package not found' }), {
         status: 404,
       });
     }
 
-    // Return the document if found
+    
     return new Response(JSON.stringify(pkg), {
       status: 200,
       headers: {
@@ -34,8 +31,7 @@ export async function GET(req,{ params }) {
     });
 
   } catch (error) {
-    // Handle any errors
-    // console.log(error)
+     
     return new Response(JSON.stringify({ error: error.message }), {
       status: 500,
     });
